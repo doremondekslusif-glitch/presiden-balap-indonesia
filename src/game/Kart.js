34fdab7c4832849b20bb760bf98f4ffb103d3cd0
doremondekslusif-glitch <doremondekslusif@gmail.com>
2026-09-19 16:58:57 +0700

    Update Kart.js

--- src/game/Kart.js
+++ src/game/Kart.js
@@ -272,7 +272,7 @@ loadCharacterModel(kart, modelPath) {
         size.z
       );
 
-      const targetSize = 4.2;
+      const targetSize = 5.0;
 
       if (maxSize > 0) {
         const scale = targetSize / maxSize;
