cc8ee70d1b87524d2cffc3b6a5708e482383e5b9
doremondekslusif-glitch <doremondekslusif@gmail.com>
2026-09-21 19:40:50 +1100

    Sertakan file musik saat deploy GitHub Pages

--- scripts/build-static.mjs
+++ scripts/build-static.mjs
@@ -11,7 +11,9 @@ const files = await readdir('.', { withFileTypes: true });
 for (const file of files) {
   if (
     file.isFile() &&
-    file.name.toLowerCase().endsWith('.glb')
+    file.name.toLowerCase().endsWith('.glb') ||
+    file.name.toLowerCase().endsWith('.mp3') ||
+    file.name.toLowerCase().endsWith('.ogg')
   ) {
     await cp(file.name, `dist/${file.name}`);
   }
